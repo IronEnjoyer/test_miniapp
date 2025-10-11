3b719cc730dff3d955660b94e6f63bb1299bce55
Kacperrr0 <106452355+IronEnjoyer@users.noreply.github.com>
2025-10-11 19:45:05 +0200

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -2,7 +2,7 @@ export async function GET() {
   const URL = process.env.NEXT_PUBLIC_URL || 'https://testminiapp-git-main-ironenjoyers-projects.vercel.app';
 
   return Response.json({
-    accountAssociation: {
+    ccountAssociation: {
       header: "eyJmaWQiOjg1MDgyOCwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDZDOGEzNDEyMTk5YTAxMjc3MDU5MGFBNjJjNzQ1NDlBNUMwY2RCQTEifQ",
       payload: "eyJkb21haW4iOiJ0ZXN0bWluaWFwcC10d28udmVyY2VsLmFwcCJ9",
       signature: "X6nXZxmJ90OJUBjkrFfcPolwEnRg8GRa5StHckZvd39Q3YpSgqmqvs7rHe7N+KtgIc8CtH2vmeuduZZLqvL5hhs="
